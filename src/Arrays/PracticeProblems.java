ec8624c0d25e71fe93f761acf059f9e26c31e7cf
developervasanthkumaran <developer.vasanthkumaran@gmail.com>
2020-08-05 13:10:08 +0530

    added additional array programs

--- src/Arrays/PracticeProblems.java
+++ src/Arrays/PracticeProblems.java
@@ -53,6 +53,28 @@ public class PracticeProblems {
         }
     }
 
+     void dutchNationalFlag(int[] a){
+        int z=0,o=0,t=0;
+        for (int value : a) {
+            if (value == 0) z++;else if(value ==1) o++;
+            else t++;
+        }
+        int s=z+o+t;
+        for(int i=0;i<s;i++){
+            if(z > 0){
+                System.out.println(0);
+                z--;
+            }
+            else if(o > 0) {
+                System.out.println(1);
+                o--;
+            }
+            else {
+                System.out.println(2);t--;
+            }
+        }
+    }
+
     void maxSubArraySum(int[] a,int target){
        HashMap<Integer,Integer> map = new HashMap<>();
        int sum=0;
@@ -79,13 +101,40 @@ public class PracticeProblems {
         }
     }
 
+    int kadaneAlgorithm(int[] a){
+        int maxSoFar = 0;
+        int max=0;
+        for(int i:a){
+            max +=i;
+            max = Math.max(max,0);
+            maxSoFar = Math.max(maxSoFar,max);
+        }
+    return  maxSoFar;
+    }
 
+    void inPlaceMergeArrayInSorted(int[] a,int[] b){
+        int m=a.length,n=b.length;
+        for(int i=0;i<m;i++){
+            if(a[i]>b[0]){
+                a[i]^=b[0];
+                b[0]^=a[i];
+                a[i]^=b[0];
+                int f = b[0],k;
+                for(k=1;k<n && b[k]<f;k++){
+                    b[k-1] = b[k];
+                }
+                b[k-1] = f;
+            }
+        }
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+    }
 
     public static void main(String[] args) {
         PracticeProblems pb = new PracticeProblems();
-        int[] a={0, 0, 1, 0, 1, 0, 0 };
-        for(int i=0;i<a.length;i++)if(a[i]==0)a[i]=-1;
-        pb.maxSubArraySum(a,0);
+        int[] a={1,4,7,8,10};
+        int[] b={2,3,6};
+            pb.inPlaceMergeArrayInSorted(a,b);
 
     }
 
