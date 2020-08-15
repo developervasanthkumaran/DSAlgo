635076e45e7143f7e6250495cef8253054fe3120
developervasanthkumaran <developer.vasanthkumaran@gmail.com>
2020-08-16 01:02:16 +0530

    updated a bug on Expression evaluation package

--- src/stack/TestStack.java
+++ src/stack/TestStack.java
@@ -4,8 +4,8 @@ public class TestStack {
 
     public static void main(String[] args) {
         Expression expression = new Expression();
-        System.out.println(expression.infixToPostfix("A+B*C/(E-F)"));
-        System.out.println(expression.infixToPrefix("A+B*C/(E-F)"));
+        System.out.println(expression.infixToPostfix("(((2*4)-(3/5)*((5*2)+8)))"));
+        System.out.println(expression.infixToPrefix("(((2*4)-(3/5)*((5*2)+8)))"));
         System.out.println(expression.evaluatePreFix("35*2+7*4-9/3"));
         System.out.println(expression.evaluatePostFix("35*2+7*4-9/3"));
 
